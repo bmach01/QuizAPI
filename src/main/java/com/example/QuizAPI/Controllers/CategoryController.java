80b330922f55b177b29e6ba398ad06c70b8027cb
Bartosz Machniak <b.machniak01@gmail.com>
2024-01-20 15:44:03 +0100

    Changed category endpoints to return responseentity

--- src/main/java/com/example/QuizAPI/Controllers/CategoryController.java
+++ src/main/java/com/example/QuizAPI/Controllers/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.QuizAPI.Controllers;
 
 import com.example.QuizAPI.DatabaseMapping.Category;
 import com.example.QuizAPI.DatabaseMapping.CategoryRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +22,17 @@ public class CategoryController {
     }
 
     @GetMapping("/title/{title}")
-    public Category getByTitle(@PathVariable String title) {
-        return repository.findByTitle(title);
+    public ResponseEntity<Category> getByTitle(@PathVariable String title) {
+        Category category = repository.findByTitle(title);
+        if (category != null) return ResponseEntity.ok(category);
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/all")
-    public List<Category> getAll() {
-        return repository.findAll();
+    public ResponseEntity<List<Category>> getAll() {
+        List<Category> categories = repository.findAll();
+        if (!categories.isEmpty()) return ResponseEntity.ok(categories);
+        return ResponseEntity.notFound().build();
     }
 
     public Category getById(String c_id) {
